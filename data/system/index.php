18a5c32918a1a96db3e1a6fdd2b69e51f4f3b984
terahidro2003 <juozas2003@gmail.com>
2023-05-11 17:06:52 +0200

    Top-nav prototype implemented

--- data/system/index.php
+++ data/system/index.php
@@ -11,67 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,500;0,600;0,700;1,400;1,500&display=swap');
-
-        * {
-            margin: 0;
-            padding: 0;
-            font-family: 'Plus Jakarta Sans', sans-serif;
-        }
-
-        .sidenav {
-            width: 12.5rem;
-            height: 100vh;
-            position: fixed;
-            background: #111130;
-
-            padding-top: 2.5rem;
-        }
-
-
-        .sidenav .logo {
-            padding-bottom: 3.438rem;
-            width: 100%;
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            margin: 0 auto;
-        }
-
-        .section {
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            margin-bottom: 3rem;
-        }
-
-        .section .section-name {
-            color: #999999;
-            opacity: .6;
-            text-transform: uppercase;
-            font-size: 0.663rem;
-            padding-left: 1.2rem;
-            margin-bottom: 0.4rem;
-        }
-
-        .section a {
-            text-decoration: none;
-            color: #ddd;
-            background-color: transparent;
-            padding: 0.75rem 1.2rem;
-            margin: 0.325rem 0;
-            font-size: 0.813rem;
-        }
-
-        .section a svg {
-            margin-right: .75rem;
-        }
-
-        .section .active {
-            background-color: #262642;
-        }
-    </style>
+    <link rel="stylesheet" href="../assets/css/ui/main.css">
     <title>Dashboard | LIMS | v0.0.1</title>
 </head>
 
@@ -130,6 +70,19 @@
                 <a href="#somewhere" class="">Configuration</a>
                 <a href="#somewhere" class="">Statistics</a>
             </div>
+        </div>
+    </div>
+    <div class="topnav">
+        <div class="search-area">
+            <input type="text" class="form-control" placeholder="Search here...">
+        </div>
+        <div>
+            <?php
+              if(isset($auth->email))
+              {
+                  echo $auth->email;
+              }   
+            ?>
         </div>
     </div>
 </body>
