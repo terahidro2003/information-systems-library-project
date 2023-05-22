9292e12a5d0c5503f0e00c216d9e697bc50f6c25
terahidro2003 <juozas2003@gmail.com>
2023-05-22 15:45:13 +0200

    CRUD: partially finished (ex. update and search)

--- data/system/library_api.php
+++ data/system/library_api.php
@@ -2,6 +2,25 @@
  error_reporting(1);
     if(isset($_REQUEST['token']))
     {
+        $fields = array(
+            'id' => isset($_REQUEST['id']) ? $_REQUEST['id'] : "null",
+            'title' => isset($_REQUEST['title']) ? $_REQUEST['title'] : "null",
+            'description' => isset($_REQUEST['description']) ? $_REQUEST['description'] : "null",
+            'quantity' => isset($_REQUEST['quantity']) ? $_REQUEST['quantity'] : "null",
+            'year_published' => isset($_REQUEST['year_published']) ? $_REQUEST['year_published'] : "null",
+            'author_group_id' => isset($_REQUEST['author_group_id']) ? $_REQUEST['author_group_id'] : "null",
+            'publisher_id' => isset($_REQUEST['publisher_id']) ? $_REQUEST['publisher_id'] : "null",
+            'added_by_user' => isset($_REQUEST['added_by_user']) ? $_REQUEST['added_by_user'] : "null",
+            'ISBN_type' => isset($_REQUEST['ISBN_type']) ? $_REQUEST['ISBN_type'] : "null",
+            'ISBN_identifier' => isset($_REQUEST['ISBN_identifier']) ? $_REQUEST['ISBN_identifier'] : "null",
+            'page_count' => isset($_REQUEST['page_count']) ? $_REQUEST['page_count'] : "null",
+            'cover_image_id' => isset($_REQUEST['cover_image_id']) ? $_REQUEST['cover_image_id'] : "null",
+            'language' => isset($_REQUEST['language']) ? $_REQUEST['language'] : "null",
+            'type' => isset($_REQUEST['type']) ? $_REQUEST['type'] : "null",
+            'created_at' => date("Y-m-d H:i:s", time()),
+            'updated_at' => date("Y-m-d H:i:s", time()),
+            'deleted_at' => date("Y-m-d H:i:s", time())
+        );
          //authentication
         require "../auth/authenticate.php";
         $auth = new Authentication();
@@ -41,28 +60,8 @@
                         }
                         break;
                     case 'insert_books':
-                        $fields = array(
-                            'id' => isset($_REQUEST['id']) ? $_REQUEST['id'] : "null",
-                            'title' => isset($_REQUEST['title']) ? $_REQUEST['title'] : "null",
-                            'description' => isset($_REQUEST['description']) ? $_REQUEST['description'] : "null",
-                            'quantity' => isset($_REQUEST['quantity']) ? $_REQUEST['quantity'] : "null",
-                            'year_published' => isset($_REQUEST['year_published']) ? $_REQUEST['year_published'] : "null",
-                            'author_group_id' => isset($_REQUEST['author_group_id']) ? $_REQUEST['author_group_id'] : "null",
-                            'publisher_id' => isset($_REQUEST['publisher_id']) ? $_REQUEST['publisher_id'] : "null",
-                            'added_by_user' => isset($_REQUEST['added_by_user']) ? $_REQUEST['added_by_user'] : "null",
-                            'ISBN_type' => isset($_REQUEST['ISBN_type']) ? $_REQUEST['ISBN_type'] : "null",
-                            'ISBN_identifier' => isset($_REQUEST['ISBN_identifier']) ? $_REQUEST['ISBN_identifier'] : "null",
-                            'page_count' => isset($_REQUEST['page_count']) ? $_REQUEST['page_count'] : "null",
-                            'cover_image_id' => isset($_REQUEST['cover_image_id']) ? $_REQUEST['cover_image_id'] : "null",
-                            'language' => isset($_REQUEST['language']) ? $_REQUEST['language'] : "null",
-                            'type' => isset($_REQUEST['type']) ? $_REQUEST['type'] : "null",
-                            'created_at' => date("Y-m-d H:i:s", time()),
-                            'updated_at' => date("Y-m-d H:i:s", time()),
-                            'deleted_at' => date("Y-m-d H:i:s", time())
-                        );
                         if($stmt = $db->con->prepare("INSERT INTO library_books (id, title, description, quantity, year_published, author_group_id, publisher_id, added_by_user, ISBN_type, ISBN_identifier, page_count, cover_image_id, language, type, created_at, updated_at, deleted_at) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"))
                         {
-                            
                             $stmt->bind_param('sssssssssssssssss', 
                                 $fields['id'], 
                                 $fields['title'],
@@ -106,6 +105,35 @@
                         }
                         break;
                     case 'edit_books':
+                        if(!isset($_REQUEST['id'])) break;
+                        if($stmt = $db->con->prepare("UPDATE library_books SET title=?, description=?, quantity=?, year_published=?, author_group_id=?, publisher_id=?, added_by_user=?, ISBN_type=?, ISBN_identifier=?, page_count=?, cover_image_id=?, language=?, type=?, created_at=?, updated_at=?, deleted_at=? WHERE id=?"))
+                        {
+                            $stmt->bind_param('sssssssssssssssss', 
+                                $fields['title'],
+                                $fields['description'],
+                                $fields['quantity'],
+                                $fields['year_published'],
+                                $fields['author_group_id'],
+                                $fields['publisher_id'],
+                                $fields['added_by_user'],
+                                $fields["ISBN_type"],
+                                $fields["ISBN_identifier"],
+                                $fields["page_count"],
+                                $fields["cover_image_id"],
+                                $fields["language"],
+                                $fields["type"],
+                                $fields["created_at"],
+                                $fields["updated_at"],
+                                $fields["deleted_at"],
+                                $_REQUEST["id"]
+                            );
+                            
+                            if($stmt->execute()) echo "SUCCESS";
+                            else echo "FAILED";
+                        }
+                        else{
+                            echo "FAILED";
+                        }
                         break;
                     default:
                         # code...
