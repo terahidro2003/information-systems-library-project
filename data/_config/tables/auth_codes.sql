6a9bbe0ad85ee3c486d355862153b993f86bfa35
terahidro2003 <juozas2003@gmail.com>
2023-06-21 15:34:01 +0200

    Optimisations

--- data/_config/tables/auth_codes.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE auth_codes(
-    id int primary key AUTO_INCREMENT,
-    code varchar(10) NOT NULL,
-    user_id int,
-    valid int,
-    created_at timestamp,
-    updated_at timestamp,
-    deleted_at timestamp
-);
--- data/_config/tables/library_authors.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE library_authors(
-    id int primary key,
-    group_id int,
-    first_name varchar(255),
-    last_name varchar(255),
-    middle_name varchar(255),
-    academic_title varchar(255)
-);
